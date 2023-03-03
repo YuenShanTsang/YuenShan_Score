1daed2d2e4b09b8c5656b4bf9eb3071461e88ff3
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-03 00:46:52 -0500

    Completed the app by adding comments for explanation

--- app/src/main/java/com/example/yuenshan_score/MainActivity.kt
+++ app/src/main/java/com/example/yuenshan_score/MainActivity.kt
@@ -2,23 +2,26 @@ package com.example.yuenshan_score
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore.Audio.Radio
 import android.widget.Button
 import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    // Team A scoreboard variable declaration
+    private lateinit var scoreATextView: TextView
     private lateinit var addButtonA: Button
     private lateinit var deductButtonA: Button
-    private lateinit var scoreATextView: TextView
 
+    // Team B scoreboard variable declaration
+    private lateinit var scoreBTextView: TextView
     private lateinit var addButtonB: Button
     private lateinit var deductButtonB: Button
-    private lateinit var scoreBTextView: TextView
 
+    // Change points display variable declaration
     private lateinit var selectPoint: RadioGroup
 
+    // Update score variable declaration
     private var teamAScore = 0
     private var teamBScore = 0
     private var changeScore = 1
@@ -27,47 +30,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize Team A views
+        scoreATextView = findViewById(R.id.scoreA_text_view)
         addButtonA = findViewById(R.id.add_buttonA)
         deductButtonA = findViewById(R.id.deduct_buttonA)
-        scoreATextView = findViewById(R.id.scoreA_text_view)
 
+        // Initialize Team B views
+        scoreBTextView = findViewById(R.id.scoreB_text_view)
         addButtonB = findViewById(R.id.add_buttonB)
         deductButtonB = findViewById(R.id.deduct_buttonB)
-        scoreBTextView = findViewById(R.id.scoreB_text_view)
 
+        // Initialize change points views
         selectPoint = findViewById(R.id.radioGroup)
 
-        selectPoint.setOnCheckedChangeListener { group, checkedId ->
+        // Change the value of changeScore based on the selected radio button
+        selectPoint.setOnCheckedChangeListener { _, checkedId ->
+            /**allows the user to select the point value for each score
+            by choosing one of the radio buttons in the radio group.**/
             when (checkedId) {
                 R.id.radio1 -> changeScore = 1
                 R.id.radio2 -> changeScore = 2
                 R.id.radio3 -> changeScore = 3
             }
         }
+
+        // Lambda function to set up the add and deduct buttons for Team A
         addButtonA.setOnClickListener{
+            // Increment the score for Team A by the changeScore value when add button is clicked
             teamAScore += changeScore
+            // Update the scoreboard for Team A
             updateTeamAScore()
         }
         deductButtonA.setOnClickListener{
+            // Decrement the score for Team A by the changeScore value when deduct button is clicked
             teamAScore -= changeScore
+            // Update the scoreboard for Team A
             updateTeamAScore()
         }
 
+        // Lambda function to set up the add and deduct buttons for Team B
         addButtonB.setOnClickListener{
+            // Increment the score for Team B by the changeScore value when add button is clicked
             teamBScore += changeScore
+            // Update the scoreboard for Team B
             updateTeamBScore()
         }
         deductButtonB.setOnClickListener{
+            // Decrement the score for Team B by the changeScore value when deduct button is clicked
             teamBScore -= changeScore
+            // Update the scoreboard for Team B
             updateTeamBScore()
         }
 
     }
 
+    // Function to update the scoreboard for Team A in the app UI
     private fun updateTeamAScore(){
+        /**To ensure that the score is within the range of 0 to 200 by using coerceIn() function
+        It prevents the score from becoming negative or exceeding the maximum limit.**/
+        teamAScore = teamAScore.coerceIn(0, 200)
+        // Display the updated score for Team A
         scoreATextView.text = teamAScore.toString()
     }
+
+    //Function to update the scoreboard for Team B in the app UI
     private fun updateTeamBScore(){
+        /**To ensure that the score is within the range of 0 to 200.
+        It prevents the score from becoming negative or exceeding the maximum limit.**/
+        teamBScore = teamBScore.coerceIn(0, 200)
+        // Display the updated score for Team B
         scoreBTextView.text = teamBScore.toString()
     }
 
